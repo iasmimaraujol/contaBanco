ee90628ee544d9826a0814f3984deba0adc30188
iasmimaraujol <iasmim.engenharia@gmail.com>
2022-06-06 14:25:32 -0300

    foi criado o metodo construtor e uma função para gerar randomicamente o numero da conta dos novos usuario

--- contaBanco.java
+++ contaBanco.java
@@ -1,3 +1,23 @@
+
+
 class contaBanco {
+    public int numConta;
+    protected String tipoConta;
+    private String donoConta;
+    private int saldoConta;
+    private boolean statusConta;
+    // contrutor
+    public contaBanco(String tipoC, String nameD, int saldoC){
+        this.numConta = digitosConta();
+        this.tipoConta = tipoC;
+        this.donoConta = nameD;
+        this.saldoConta = saldoC;
+        this.statusConta = true;
+    }
+    public int digitosConta(){
+        int num = (int)(1000*Math.random());
+        return num;
+    }
+
     
 }
